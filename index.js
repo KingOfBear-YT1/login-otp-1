d1b4266f0b643c6193d4b3b7939ba5b769c9aa17
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-10-24 00:54:53 +0700

    p

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+// memanggil modul http
+const http = require('http');
+
+// memanggil modul fs
+const fs = require('fs');
+
+// membuat fungsi panggil HTML
+function panggilPHP(path, res){
+    fs.readFile(path, 'utf8', function(err, data){
+        // jika error
+        if(err){
+            res.writeHead(404);
+            res.write('<h1>File tidak ditemukan!</h1>');
+        }
+        else{
+            res.write(data);
+        }
+        // untuk mengakhiri response 
+        res.end();
+    });
+}
+
+// membuat server
+const server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type' : 'text/php'});
+
+    // memanggil fungsi panggilHTML
+    panggilPHP('./login-user.php',res);  
+});
+
+server.listen(3000);
